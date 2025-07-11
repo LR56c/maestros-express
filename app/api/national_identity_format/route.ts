080d83f391d1c13f6cba387f9938188fbb4920b8
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-07-11 13:37:13 -0400

    update dependencies to deploy

--- app/api/national_identity_format/route.ts
+++ app/api/national_identity_format/route.ts
@@ -1,7 +1,6 @@
 "use server"
 
 import { NextRequest, NextResponse } from "next/server"
-import prisma                        from "@/lib/prisma"
 import {
   querySchema
 }                                    from "@/modules/shared/application/query_dto"
@@ -10,31 +9,17 @@ import {
 }                                    from "@/modules/shared/application/parse_handlers"
 import { isLeft }                    from "fp-ts/Either"
 import {
-  PrismaNationalIdentityFormatData
-}                                    from "@/modules/national_identity_format/infrastructure/prisma_national_identity_format_data"
-import {
-  AddNationalIdentityFormat
-}                                    from "@/modules/national_identity_format/application/add_national_identity_format"
-import {
-  RemoveNationalIdentityFormat
-}                                    from "@/modules/national_identity_format/application/remove_national_identity_format"
-import {
-  UpdateNationalIdentityFormat
-}                                    from "@/modules/national_identity_format/application/update_national_identity_format"
-import {
-  SearchNationalIdentityFormat
-}                                    from "@/modules/national_identity_format/application/search_national_identity_format"
-import { searchCountry }             from "@/app/api/country/route"
-import {
   nationalIdentityFormatSchema
 }                                    from "@/modules/national_identity_format/application/national_identity_format_dto"
 import {
   NationalIdentityFormatMapper
 }                                    from "@/modules/national_identity_format/application/national_identity_format_mapper"
 import {
-  addNationalIdentity, removeNationalIdentity,
-  searchNationalIdentityFormat, updateNationalIdentity
-} from "@/app/api/dependencies"
+  addNationalIdentity,
+  removeNationalIdentity,
+  searchNationalIdentityFormat,
+  updateNationalIdentity
+}                                    from "@/app/api/dependencies"
 
 export async function POST( request: NextRequest ) {
   const body = await request.json()
@@ -44,7 +29,9 @@ export async function POST( request: NextRequest ) {
     return NextResponse.json( { error: data.left.message }, { status: 400 } )
   }
 
-  const result = await (await addNationalIdentity()).execute( data.right )
+  const result = await (
+    await addNationalIdentity()
+  ).execute( data.right )
 
   if ( isLeft( result ) ) {
     return NextResponse.json( { status: 500 } )
@@ -72,7 +59,9 @@ export async function GET( request: NextRequest ) {
     return NextResponse.json( { error: data.left.message }, { status: 400 } )
   }
 
-  const result = await (await searchNationalIdentityFormat()).execute(
+  const result = await (
+    await searchNationalIdentityFormat()
+  ).execute(
     data.right.query,
     data.right.limit,
     data.right.skip,
@@ -97,7 +86,9 @@ export async function PUT( request: NextRequest ) {
     return NextResponse.json( { error: data.left.message }, { status: 400 } )
   }
 
-  const result = await (await updateNationalIdentity()).execute( data.right )
+  const result = await (
+    await updateNationalIdentity()
+  ).execute( data.right )
 
   if ( isLeft( result ) ) {
     return NextResponse.json( { status: 500 } )
@@ -111,7 +102,9 @@ export async function DELETE( request: NextRequest ) {
   const { searchParams } = new URL( request.url )
   const id               = searchParams.get( "id" )
 
-  const result = await (await removeNationalIdentity()).execute( id ?? "" )
+  const result = await (
+    await removeNationalIdentity()
+  ).execute( id ?? "" )
 
   if ( isLeft( result ) ) {
     return NextResponse.json( { status: 500 } )
