03482b239a739d6aef042f0154d76f7ffb378127
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-07-11 17:26:57 -0400

    fix prisma chat

--- modules/chat/infrastructure/prisma_chat_data.ts
+++ modules/chat/infrastructure/prisma_chat_data.ts
@@ -1,7 +1,9 @@
 import { PrismaClient }        from "@/lib/generated/prisma"
 import { ChatDAO }             from "@/modules/chat/domain/chat_dao"
 import { Chat }                from "@/modules/chat/domain/chat"
-import { UUID }                from "@/modules/shared/domain/value_objects/uuid"
+import {
+  UUID
+}                              from "@/modules/shared/domain/value_objects/uuid"
 import { Either, left, right } from "fp-ts/Either"
 import {
   BaseException
@@ -9,10 +11,14 @@ import {
 import {
   InfrastructureException
 }                              from "@/modules/shared/domain/exceptions/infrastructure_exception"
-import { Errors }              from "@/modules/shared/domain/exceptions/errors"
+import {
+  Errors
+}                              from "@/modules/shared/domain/exceptions/errors"
 import {
   DataNotFoundException
-} from "@/modules/shared/domain/exceptions/data_not_found_exception"
+}                              from "@/modules/shared/domain/exceptions/data_not_found_exception"
+import { UserAuth }            from "@/modules/user/domain/user"
+import { Message }             from "@/modules/message/domain/message"
 
 export class PrismaChatData implements ChatDAO {
   constructor( private readonly db: PrismaClient ) {
@@ -34,8 +40,8 @@ export class PrismaChatData implements ChatDAO {
             ? chat.workerArchived.value
             : null,
           createdAt        : chat.createdAt.toString(),
-          workerId         : chat.workerId.toString(),
-          clientId         : chat.clientId.toString()
+          workerId         : chat.worker.userId.toString(),
+          clientId         : chat.client.userId.toString()
         }
       } )
       return right( true )
@@ -45,11 +51,87 @@ export class PrismaChatData implements ChatDAO {
     }
   }
 
+  private mapToChat( response: any ): Chat | Errors {
+    const worker       = response.worker
+    const workerMapped = UserAuth.fromPrimitives(
+      worker.id,
+      worker.email,
+      worker.name,
+      worker.createdAt,
+      worker.role,
+      worker.status,
+      worker.avatar
+    )
+
+    if ( workerMapped instanceof Errors ) {
+      return left( workerMapped.values )
+    }
+
+    const client       = response.client
+    const clientMapped = UserAuth.fromPrimitives(
+      client.id,
+      client.email,
+      client.name,
+      client.createdAt,
+      client.role,
+      client.status,
+      client.avatar
+    )
+
+    if ( clientMapped instanceof Errors ) {
+      return left( clientMapped.values )
+    }
+
+    const messages: Message[] = []
+
+    for ( const m of response.messages ) {
+      const message = Message.fromPrimitives(
+        m.id,
+        m.chatId,
+        m.userId,
+        m.content,
+        m.type,
+        m.status,
+        m.createdAt
+      )
+
+      if ( message instanceof Errors ) {
+        return left( message.values )
+      }
+
+      messages.push( message )
+    }
+
+
+    const chat = Chat.fromPrimitives(
+      response.id,
+      workerMapped,
+      clientMapped,
+      response.createdAt,
+      response.subject,
+      messages,
+      response.acceptedDate ? response.acceptedDate : undefined,
+      response.quotationAccepted ? response.quotationAccepted : undefined,
+      response.workerArchived ? response.workerArchived : undefined
+    )
+
+    if ( chat instanceof Errors ) {
+      return left( chat.values )
+    }
+
+    return chat
+  }
+
   async getById( id: UUID ): Promise<Either<BaseException[], Chat>> {
     try {
       const response = await this.db.chat.findUnique( {
-        where: {
+        where  : {
           id: id.toString()
+        },
+        include: {
+          worker  : true,
+          client  : true,
+          messages: true
         }
       } )
 
@@ -57,22 +139,13 @@ export class PrismaChatData implements ChatDAO {
         return left( [new DataNotFoundException()] )
       }
 
-      const chat = Chat.fromPrimitives(
-        response.id,
-        response.workerId,
-        response.clientId,
-        response.createdAt,
-        response.subject,
-        response.acceptedDate ? response.acceptedDate : undefined,
-        response.quotationAccepted ? response.quotationAccepted : undefined,
-        response.workerArchived ? response.workerArchived : undefined
-      )
+      const mapped = this.mapToChat( response )
 
-      if ( chat instanceof Errors ) {
-        return left( chat.values )
+      if ( mapped instanceof Errors ) {
+        return left( mapped.values )
       }
 
-      return right( chat )
+      return right( mapped )
     }
     catch ( e ) {
       return left( [new InfrastructureException()] )
@@ -81,30 +154,24 @@ export class PrismaChatData implements ChatDAO {
 
   async getByUser( userId: UUID ): Promise<Either<BaseException[], Chat[]>> {
     try {
-      const response = await this.db.chat.findMany( {
-        where: {
+      const result = await this.db.chat.findMany( {
+        where  : {
           clientId: userId.toString()
+        },
+        include: {
+          worker  : true,
+          client  : true,
+          messages: true
         }
       } )
 
       const chats: Chat[] = []
-      for ( const e of response ) {
-        const chat = Chat.fromPrimitives(
-          e.id,
-          e.workerId,
-          e.clientId,
-          e.createdAt,
-          e.subject,
-          e.acceptedDate ? e.acceptedDate : undefined,
-          e.quotationAccepted ? e.quotationAccepted : undefined,
-          e.workerArchived ? e.workerArchived : undefined
-        )
+      for ( const response of result ) {
+        const mapped = this.mapToChat( response )
 
-        if ( chat instanceof Errors ) {
-          return left( chat.values )
+        if ( mapped instanceof Errors ) {
+          return left( mapped.values )
         }
-
-        chats.push( chat )
       }
 
       return right( chats )
@@ -120,9 +187,15 @@ export class PrismaChatData implements ChatDAO {
         where: { id: chat.id.toString() },
         data : {
           subject          : chat.subject?.value,
-          quotationAccepted: chat.quotationAccepted ? chat.quotationAccepted.value : null,
-          acceptedDate     : chat.acceptedDate ? chat.acceptedDate.toString() : null,
-          workerArchived   : chat.workerArchived ? chat.workerArchived.value : null
+          quotationAccepted: chat.quotationAccepted
+            ? chat.quotationAccepted.value
+            : null,
+          acceptedDate     : chat.acceptedDate
+            ? chat.acceptedDate.toString()
+            : null,
+          workerArchived   : chat.workerArchived
+            ? chat.workerArchived.value
+            : null
         }
       } )
       return right( true )
