1cc42d738cd214423f41740a8a505199c4f8cafe
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-07-12 09:24:42 -0400

    fix embedding typo

--- modules/worker_embedding/application/worker_embedding_app_service.ts
+++ modules/worker_embedding/application/worker_embedding_app_service.ts
@@ -1,12 +1,15 @@
 import {
-  WorkerEmbeddingDTO
+  WorkerEmbeddingResponse
 } from "@/modules/worker_embedding/application/worker_embedding_response"
+import {
+  WorkerEmbeddingRequest
+} from "@/modules/worker_embedding/application/worker_embedding_request"
 
 
 export abstract class WorkerEmbeddingAppService {
-  abstract add( embed : WorkerEmbeddingDTO ): Promise<void>
+  abstract add( embed: WorkerEmbeddingRequest ): Promise<void>
 
   abstract remove( id: string ): Promise<void>
 
-  abstract search( query: string ): Promise<WorkerEmbeddingDTO[]>
+  abstract search( query: string ): Promise<WorkerEmbeddingResponse[]>
 }
