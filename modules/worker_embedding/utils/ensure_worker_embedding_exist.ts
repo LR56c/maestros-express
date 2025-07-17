4ade37525e1d8aff44e1997cd0ecdbcbba57654f
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-07-17 12:22:34 -0400

    adjust type

--- modules/worker_embedding/utils/ensure_worker_embedding_exist.ts
+++ modules/worker_embedding/utils/ensure_worker_embedding_exist.ts
@@ -1,42 +1,39 @@
-import { PaymentDAO } from "@/modules/payment/domain/payment_dao"
 import { Either, isLeft, left, right } from "fp-ts/Either"
 import {
   BaseException
-} from "@/modules/shared/domain/exceptions/base_exception"
-import { Payment } from "@/modules/payment/domain/payment"
-import {
-  ValidInteger
-} from "@/modules/shared/domain/value_objects/valid_integer"
+}                                      from "@/modules/shared/domain/exceptions/base_exception"
 import {
   DataNotFoundException
-} from "@/modules/shared/domain/exceptions/data_not_found_exception"
+}                                      from "@/modules/shared/domain/exceptions/data_not_found_exception"
 import {
   WorkerEmbeddingRepository
 }                                      from "@/modules/worker_embedding/domain/worker_embedding_repository"
 import {
   WorkerEmbedding
-} from "@/modules/worker_embedding/domain/worker_embedding"
-import { wrapType } from "@/modules/shared/utils/wrap_type"
-import { UUID } from "@/modules/shared/domain/value_objects/uuid"
+}                                      from "@/modules/worker_embedding/domain/worker_embedding"
+import { wrapType }                    from "@/modules/shared/utils/wrap_type"
+import {
+  UUID
+}                                      from "@/modules/shared/domain/value_objects/uuid"
 
 export const ensureWorkerEmbeddingExist = async ( repo: WorkerEmbeddingRepository,
   embedId: string ): Promise<Either<BaseException[], WorkerEmbedding>> => {
 
-  const vEmbedId = wrapType(()=>UUID.from(embedId))
+  const vEmbedId = wrapType( () => UUID.from( embedId ) )
 
-  if ( vEmbedId instanceof  BaseException) {
-    return left([vEmbedId])
+  if ( vEmbedId instanceof BaseException ) {
+    return left( [vEmbedId] )
   }
 
-  const embed = await repo.getById(vEmbedId)
+  const embed = await repo.getById( vEmbedId )
 
-  if ( isLeft(embed) ) {
-    return left(embed.left)
+  if ( isLeft( embed ) ) {
+    return left( embed.left )
   }
 
-  if ( embed.right.id.value !== embedId ) {
+  if ( embed.right[0].id.toString() !== embedId ) {
     return left( [new DataNotFoundException()] )
   }
 
-  return right(embed.right)
+  return right( embed.right[0] )
 }
